0f71a51d21b62b4470767729f44d10c4e631d9db
AsherLMac <renpsybeta@gmail.com>
2018-07-05 16:50:10 -0400

    Finished Error Handling

--- game/index.js
+++ game/index.js
@@ -3,22 +3,124 @@ import { Map } from 'immutable'
 //Empty Initial Board
 const board = Map()
 
+const winstates = {
+  top: ['tl', 'tm', 'tr'],
+  mid: ['ml', 'mm', 'mr'],
+  bot: ['bl', 'bm', 'br'],
+  left: ['ll', 'lm', 'lr'],
+  center: ['cl', 'cm', 'cr'],
+  right: ['rl', 'rm', 'rr'],
+  tb: ['tt', 'cc', 'bb'],
+  bt: ['bb', 'cc', 'tt'],
+}
+
+const checkHoz = function (move, player) {
+  switch (move[0]) {
+    case 0:
+      winstates.top[move[1]] = player
+      return winstates.top.every(elem => { return elem === player })
+    case 1:
+      winstates.mid[move[1]] = player
+      return winstates.mid.every(elem => { return elem === player })
+    case 2:
+      winstates.bot[move[1]] = player
+      return winstates.bot.every(elem => { return elem === player })
+    default:
+      return false
+  }
+}
+
+const checkVer = function (move, player) {
+  switch (move[1]) {
+    case 0:
+      winstates.left[move[0]] = player
+      return winstates.left.every(elem => { return elem === player })
+    case 1:
+      winstates.center[move[0]] = player
+      return winstates.center.every(elem => { return elem === player })
+    case 2:
+      winstates.right[move[0]] = player
+      return winstates.right.every(elem => { return elem === player })
+    default:
+      return false
+  }
+}
+
+const checkDaiTB = function (move, player) {
+  switch (move[0]) {
+    case 0:
+      if (move[1] === 0) winstates.tb[0] = player
+      return winstates.tb.every(elem => { return elem === player })
+    case 1:
+      if (move[1] === 1) winstates.tb[1] = player
+      return winstates.tb.every(elem => { return elem === player })
+    case 2:
+      if (move[1] === 2) winstates.tb[2] = player
+      return winstates.tb.every(elem => { return elem === player })
+    default:
+      return false
+  }
+}
+
+const checkDaiBT = function (move, player) {
+  switch (move[0]) {
+    case 0:
+      if (move[1] === 2) winstates.bt[0] = player
+      return winstates.bt.every(elem => { return elem === player })
+    case 1:
+      if (move[1] === 1) winstates.bt[1] = player
+      return winstates.bt.every(elem => { return elem === player })
+    case 2:
+      if (move[1] === 0) winstates.bt[2] = player
+      return winstates.bt.every(elem => { return elem === player })
+    default:
+      return false
+  }
+}
+
 const move = function (aTurn, coor) {
   //Makes Move Action Object
   const action = {
     type: 'move', //Type
     turn: aTurn, //Player
     name: coor, //Position
-    message: `${aTurn}'s moved: ${coor}` //Message Of Move
+    message: `${aTurn} Move: ${coor}` //Message Of Move
   }
   return action
 }
 
+const errorCheck = function (action) {
+  //Checks Action For Legit Inputs, Between 0 & 2
+  let isBad = false
+  action.name.forEach(element => {
+    if (element < 0 || element > 2) {
+      isBad = true
+    }
+  })
+  return isBad
+}
+
 //Exporting Reducer for Redux
-function reducer(state = { board, turn: 'X' }, action) {
+function reducer(state = { board, turn: 'X', winner: null }, action) {
   switch (action.type) {
     case 'move':
-      state.board = state.board.setIn(action.name, action.turn)
+      if (errorCheck(action)) {
+        console.log(action.message + ' Is Invalid')
+        return state
+      }
+
+      if (!state.board.getIn(action.name)) {
+        state.board = state.board.setIn(action.name, action.turn)
+      } else {
+        console.log(action.message + ' Is Invalid, Position Is Filled')
+        return state
+      }
+
+      //Check Conditions
+      if (checkHoz(action.name, state.turn) || checkVer(action.name, state.turn) || checkDaiTB(action.name, state.turn) || checkDaiBT(action.name, state.turn)) {
+        state.winner = action.turn
+      }
+      //Swap Players
       if (state.turn === 'X') {
         state.turn = 'O'
       } else {
@@ -26,6 +128,7 @@ function reducer(state = { board, turn: 'X' }, action) {
       }
       console.log(action.message)
       return state
+
     default:
       return state
   }
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import inquirer from 'inquirer'
 
-import {reducer, move} from './game'
-import {createStore} from 'redux'
+import { reducer, move } from './game'
+import { createStore } from 'redux'
 
 
 // Create the store
 const game = createStore(reducer)
 
 const printBoard = () => {
-  const {board} = game.getState()
+  const { board } = game.getState()
   for (let r = 0; r != 3; ++r) {
     for (let c = 0; c != 3; ++c) {
       process.stdout.write(board.getIn([r, c], '_'))
@@ -17,15 +17,23 @@ const printBoard = () => {
   }
 }
 
+const printWinner = () => {
+  const { winner } = game.getState()
+  if (winner) {
+    console.log(winner + ' Wins!')
+    process.exit()
+  }
+}
+
 const getInput = player => async () => {
-  const {turn} = game.getState()  
+  const { turn } = game.getState()
   if (turn !== player) return
   const ans = await inquirer.prompt([{
     type: 'input',
     name: 'coord',
     message: `${turn}'s move (row,col):`
   }])
-  const [row=0, col=0] = ans.coord.split(/[,\s+]/).map(x => +x)
+  const [row = 0, col = 0] = ans.coord.split(/[,\s+]/).map(x => +x)
   game.dispatch(move(turn, [row, col]))
 }
 
@@ -33,6 +41,7 @@ const getInput = player => async () => {
 // game.subscribe(() => console.log(game.getState()))
 
 game.subscribe(printBoard)
+game.subscribe(printWinner)
 game.subscribe(getInput('X'))
 game.subscribe(getInput('O'))
 
